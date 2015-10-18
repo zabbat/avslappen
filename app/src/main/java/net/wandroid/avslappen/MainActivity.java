ddf0785fdd777ab87ec29be5b9d53bcf86705bb2
johan Jungbeck <johan.jungbeck@wandroid.net>
2015-10-18 14:27:50 +0200

    Adding paralax scroll

--- app/src/main/java/net/wandroid/avslappen/MainActivity.java
+++ app/src/main/java/net/wandroid/avslappen/MainActivity.java
@@ -1,15 +1,16 @@
 package net.wandroid.avslappen;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import net.wandroid.avslappen.views.ChapterAdapter;
 
@@ -25,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle(toolbar.getTitle());
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener((View view) ->
@@ -32,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show());
 
         mRecyclerView = (RecyclerView) findViewById(R.id.chapter_list_view);
-        mLayoutManager= new LinearLayoutManager(this);
+        mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
         mAdapter = new ChapterAdapter();
         mRecyclerView.setAdapter(mAdapter);
