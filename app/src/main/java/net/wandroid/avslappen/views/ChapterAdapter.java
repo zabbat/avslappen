29fbbba16b7cfe9000c5b6075766d83b6bb164c9
johan Jungbeck <johan.jungbeck@wandroid.net>
2015-10-18 13:36:53 +0200

    adjusting chapter list tiles
    The items should follow material design standard.
    added dummy text and a dummy thumbnail that should
    be replacedwith real chapter info later.

--- app/src/main/java/net/wandroid/avslappen/views/ChapterAdapter.java
+++ app/src/main/java/net/wandroid/avslappen/views/ChapterAdapter.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import net.wandroid.avslappen.R;
@@ -21,13 +22,14 @@ public class ChapterAdapter extends RecyclerView.Adapter<ChapterAdapter.ViewHold
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.chapter_list_item,parent,false);
         ViewHolder viewHolder= new ViewHolder(view);
-        viewHolder.mTitle.setText("here");
         return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         holder.mTitle.setText("position:"+position);
+        holder.mInfo.setText("secondary line");
+        holder.mThumbnail.setImageResource(R.drawable.tmp_thumb_flowered_riverbank);
     }
 
     @Override
@@ -38,11 +40,15 @@ public class ChapterAdapter extends RecyclerView.Adapter<ChapterAdapter.ViewHold
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
         public TextView mTitle;
+        public TextView mInfo;
+        public ImageView mThumbnail;
         public View mView;
         public ViewHolder(View itemView) {
             super(itemView);
             mView = itemView;
             mTitle = (TextView) mView.findViewById(R.id.list_item_title);
+            mInfo = (TextView) mView.findViewById(R.id.list_item_info_text);
+            mThumbnail = (ImageView) mView.findViewById(R.id.list_item_thumbnail);
         }
     }
 
