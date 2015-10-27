3670645f03be1239b4d50ed6b23cc8d018b2e18e
Johan Jungbeck <johan.jungbeck@wandroid.net>
2015-10-27 05:26:05 -0400

    Adding Chapter info activity
    Adding an activity that will display information
    about a selected chapter. By pressing an item
    in the list this activity will be launched

--- app/src/main/java/net/wandroid/avslappen/views/ChapterAdapter.java
+++ app/src/main/java/net/wandroid/avslappen/views/ChapterAdapter.java
@@ -1,5 +1,7 @@
 package net.wandroid.avslappen.views;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,9 +9,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import net.wandroid.avslappen.ChapterInfoActivity;
 import net.wandroid.avslappen.R;
-
-import java.util.zip.Inflater;
 
 /**
  * Created by zabbat on 2015-10-18.
@@ -20,17 +21,22 @@ public class ChapterAdapter extends RecyclerView.Adapter<ChapterAdapter.ViewHold
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.chapter_list_item,parent,false);
-        ViewHolder viewHolder= new ViewHolder(view);
+        Context context = parent.getContext();
+        View view = LayoutInflater.from(context).inflate(R.layout.chapter_list_item, parent, false);
+        view.setOnClickListener((View v) ->
+                    context.startActivity(new Intent(context, ChapterInfoActivity.class))
+        );
+        ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.mTitle.setText("position:"+position);
+        holder.mTitle.setText("position:" + position);
         holder.mInfo.setText("secondary line");
         holder.mThumbnail.setImageResource(R.drawable.tmp_thumb_flowered_riverbank);
     }
+
 
     @Override
     public int getItemCount() {
@@ -43,6 +49,7 @@ public class ChapterAdapter extends RecyclerView.Adapter<ChapterAdapter.ViewHold
         public TextView mInfo;
         public ImageView mThumbnail;
         public View mView;
+
         public ViewHolder(View itemView) {
             super(itemView);
             mView = itemView;
