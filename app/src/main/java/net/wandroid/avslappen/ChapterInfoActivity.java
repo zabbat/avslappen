bd41a2bfeb36d032b5a71425fc51b9c4684e1e84
Johan Jungbeck <johan.jungbeck@wandroid.net>
2015-10-28 04:50:18 -0400

    Simple MediaPlayer added
    By pressing play button a file
    "sdcard/media/audio/chap1.mp3" is played
    through the MediaPlayer interface.
    
    The file can be found under external_content/
    
    It will be stopped if the activity is stopped (onStop)

--- app/src/main/java/net/wandroid/avslappen/ChapterInfoActivity.java
+++ app/src/main/java/net/wandroid/avslappen/ChapterInfoActivity.java
@@ -1,14 +1,68 @@
 package net.wandroid.avslappen;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ChapterInfoActivity extends AppCompatActivity {
+
+    public static final String PATH_CHAP1_MP3 = "/media/audio/chap1.mp3";
+    private ImageView mPlayIcon;
+    private MediaPlayer mMediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chapter_info);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mPlayIcon = (ImageView) findViewById(R.id.play_icon);
+        mPlayIcon.setOnClickListener((View v) -> {
+            String external = Environment.getExternalStorageDirectory().toString();
+            File mp3Path = new File(external + PATH_CHAP1_MP3);
+            if (mp3Path.exists()) {
+                startAudio(mp3Path);
+            } else {
+                //TODO: Currently you need to push chap1.mp3 to a folder in sdcard/media/audio
+                Toast.makeText(ChapterInfoActivity.this, "Could not find: " + mp3Path.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void startAudio(File audioFile) {
+        Uri uri = Uri.fromFile(audioFile);
+        mMediaPlayer = new MediaPlayer();
+        mMediaPlayer.setOnPreparedListener((MediaPlayer mp) -> mp.start());
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            mMediaPlayer.setDataSource(getApplicationContext(), uri);
+            mMediaPlayer.prepareAsync();
+        } catch (IOException | IllegalArgumentException e) {
+            e.printStackTrace();
+            Toast.makeText(ChapterInfoActivity.this, "failed to start audio",
+                    Toast.LENGTH_SHORT).show();
+            mMediaPlayer.release();
+        }
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        if(mMediaPlayer!=null){
+            mMediaPlayer.stop();
+            mMediaPlayer.release();
+        }
+        super.onStop();
     }
 }
